ca3a5750920e3e59f9318f86d83061c731ffc086
vrdlc <virgilioramon.delacruz@gmail.com>
2016-09-07 14:48:15 -0700

    fix jump

---
+++ Assets/Scripts/CubeBehaviorScript.cs
@@ -0,0 +1,57 @@
+
+using UnityEngine;
+using System.Collections;
+
+public class CubeBehaviorScript : MonoBehaviour {
+
+	[HideInInspector] public bool facingRight = true;
+	[HideInInspector] public bool jump = true;
+
+	public float moveForce = 365f;
+	public float maxSpeed = 5f;
+	public float jumpForce = 1000f;
+	public Transform groundCheck;
+
+	private bool grounded = false;
+
+	private Rigidbody2D rb2d;
+
+
+	// Use this for initialization
+	void Awake () {
+
+		rb2d = GetComponent<Rigidbody2D>();
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+
+		if (Input.GetButtonDown("Jump") && grounded) {
+			jump = true;
+		}
+
+	}
+
+	void FixedUpdate() {
+		float h = Input.GetAxis("Horizontal");
+
+		if (rb2d.velocity.y > 0) {
+			Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Player"), LayerMask.NameToLayer("Ground"), true);
+		} else {
+			Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Player"), LayerMask.NameToLayer("Ground"), false);
+		}
+
+		if (h * rb2d.velocity.x < maxSpeed)
+		rb2d.AddForce(Vector2.right * h * moveForce);
+
+		if (Mathf.Abs (rb2d.velocity.x) > maxSpeed)
+		rb2d.velocity = new Vector2 (Mathf.Sign(rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
+
+		if (jump) {
+			rb2d.AddForce(new Vector2(0f, jumpForce));
+			jump = false;
+		}
+	}
+}
